b099299ab1d6ccfabdc28737eca673323b908e69
Alperen Elhan <alperen@elhan.io>
2019-09-30 23:55:55 +0300

    feat: add global shortcut, open window in current display

--- src/electron.js
+++ src/electron.js
@@ -3,12 +3,15 @@ const electron = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
 
-const { BrowserWindow, app } = electron;
+const { BrowserWindow, app, globalShortcut } = electron;
 
 let bgWindow;
 let mainWindow;
+let screen;
 
 function createWindow() {
+  screen = electron.screen;
+
   bgWindow = new BrowserWindow({
     width: 0,
     height: 0,
@@ -21,14 +24,15 @@ function createWindow() {
     skipTaskbar: true,
     focusable: false,
     fullscreenable: false,
-    title: 'Kitsu Launcher',
     autoResize: true,
   });
+
   mainWindow = new BrowserWindow({
     width: 600,
     height: 500,
     frame: false,
     resizable: false,
+    show: false,
     transparent: true,
     minimizable: false,
     maximizable: false,
@@ -50,7 +54,9 @@ function createWindow() {
   });
 }
 
-app.on('ready', () => setTimeout(createWindow, 500));
+app.on('ready', () => {
+  setTimeout(createWindow, 500);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -62,4 +68,31 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
+});
+
+app.on('ready', () => {
+  // Register a 'Alt+Space' shortcut listener.
+  globalShortcut.register('Alt+Space', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      const currentDisplay = screen.getDisplayNearestPoint(
+        screen.getCursorScreenPoint()
+      );
+      mainWindow.setPosition(
+        currentDisplay.bounds.x + 50,
+        currentDisplay.bounds.y + 50
+      );
+      mainWindow.center();
+      mainWindow.show();
+    }
+  });
+});
+
+app.on('will-quit', () => {
+  // Unregister a shortcut.
+  globalShortcut.unregister('Alt+Space');
+
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll();
 });
