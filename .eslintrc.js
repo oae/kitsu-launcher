4de8b58ff6ef1803a8de36325fa322dcf1f00c1c
Alperen Elhan <alperen@elhan.io>
2019-10-13 01:25:54 +0300

    chore: add styled-components and storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: ['react', 'jest', 'prettier'],
+  plugins: ['react', 'react-hooks', 'jest', 'prettier'],
   rules: {
     'arrow-parens': 0,
     'react/prop-types': 0,
@@ -13,8 +13,10 @@ module.exports = {
     'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true }],
     'import/prefer-default-export': 0,
     'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/no-autofocus': 0,
     'jsx-a11y/alt-text': 0,
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
   settings: {
     'import/core-modules': ['electron'],
